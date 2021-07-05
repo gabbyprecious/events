d7c2e441f2e4c9ceee6ff641c7d1062e83017cff
GabbyPrecious <gabbyprecious2000@gmail.com>
2021-07-05 15:41:56 +0100

    added validations and some changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,16 @@
 class Event < ApplicationRecord
     belongs_to :user
 
+    before_save do
+        if event_is_free?
+            self.price = 0
+        end
+    end
+
     validates_presence_of :title, :status, :description
+    validates :price, absence: {message: "must be empty if event is free please" }, if: :event_is_free?
+
+    def event_is_free?
+        status == "free"
+    end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_111358) do
+ActiveRecord::Schema.define(version: 2021_07_05_134449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_07_05_111358) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.boolean "active", default: true
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
