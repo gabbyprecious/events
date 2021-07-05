45bcf0caf6996d076e49938f4b723defbd0ef7ea
GabbyPrecious <gabbyprecious2000@gmail.com>
2021-07-05 16:40:29 +0100

    added edit and delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,6 @@ class EventsController < ApplicationController
     end
 
     def create
-        # @event = current_user.events.new(event_params)
         @event = Event.new(event_params.merge(user_id: current_user.id))
 
         if @event.save
@@ -22,8 +21,38 @@ class EventsController < ApplicationController
         end
     end
 
+    def edit
+        @event = Event.find_by(id: params[:id], user_id:current_user.id)
+    end
+
+    def update
+        @event = Event.find_by(id: params[:id], user_id:current_user.id)
+
+        if @event.update(event_params)
+            redirect_to @event
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+
+        redirect_to root_path
+    end
+
+    def update_active
+        @event = Event.find(params[:id])
+        if event.active
+            @event.update(:active, false)
+        else
+            @event.update(:active, false)
+        end
+    end
+
     private
     def event_params
-      params.require(:event).permit(:title, :description, :status, :price)
+      params.require(:event).permit(:title, :description, :status, :price, :active)
     end
 end
